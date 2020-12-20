81e276918eeb7e19a996d21017ad70b949f06063
u25j91 <u25j91@dergbsl1072.avl01.avlcorp.lan>
2020-12-20 17:16:18 +0100

    Forms and 404 page

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 app =  Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('basic.html')
+    return render_template('index.html')
 
 @app.route('/other')
-def other():
-    return render_template('test.html')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/thank')
+def thank_you():
+    first = request.args.get('first')
+    last = request.args.get('last')
+    return render_template('thankyou.html', first=first, last=last)
     
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True,host= '0.0.0.0')
